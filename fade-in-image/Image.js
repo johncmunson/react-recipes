e49be4cbb72a28b0b02265454207063ca6329efc
John Munson <johncmunson91@gmail.com>
2016-12-16 00:22:06 -0700

    added fade-in-image component

---
+++ fade-in-image/Image.js
@@ -0,0 +1,46 @@
+// Want an easy way to make your images a little more graceful?
+// This recipe is for an Image component that fades in once the image is loaded.
+// Getting images to fade becomes a matter of replacing <img> with <Image>.
+
+
+var classNames = require('classnames');
+
+var Image = React.createClass({
+  getInitialState: function() {
+    return {
+      loaded: false
+    };
+  },
+
+  onImageLoad: function() {
+    if (this.isMounted()) {
+      this.setState({ loaded: true });
+    }
+  },
+
+  componentDidMount: function() {
+    var imgTag = ReactDOM.findDOMNode(this.refs.img);
+    var imgSrc = imgTag.getAttribute('src');
+    // You may want to rename the component if the <Image> definition
+    // overrides window.Image
+    var img = new window.Image();
+    img.onload = this.onImageLoad;
+    img.src = imgSrc;
+  },
+
+  render: function() {
+    var { className, ...props } = this.props;
+    var imgClasses = 'image';
+    var rootClassName = classNames(className, 'image', {
+      'image-loaded': this.state.loaded,
+    });
+    return (
+      <img ref="img" {...props} className={rootClassName} />
+    );
+  }
+});
+
+ReactDOM.render(
+  <Image src="path/to/image" />,
+  node
+);
